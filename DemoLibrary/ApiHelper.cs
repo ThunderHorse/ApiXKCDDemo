ad9d746eb95beefe20daf3cbcf9d7f950ee0ea40
Pease <jason.pease@aprimo.com>
2018-10-09 13:14:27 -0400

    first commit

---
+++ DemoLibrary/ApiHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+	public class ApiHelper
+	{
+		public static HttpClient ApiClient { get; set; }
+
+		public static void InitializeClient()
+		{
+			ApiClient = new HttpClient();
+			//ApiClient = new Uri("http://xkcd.com/"); // Not using because we will use more than one base client for our requests
+			ApiClient.DefaultRequestHeaders.Accept.Clear();
+			ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		}
+	}
+}
